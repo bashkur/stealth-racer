09401bdc4842e844996183d91cbddb88a7146c20
Crusher48 <Anthonywc117@sbcglobal.net>
2019-12-05 13:32:59 -0500

    Added timer UI and checkpoints
    Implemented a timer UI and a checkpoint system. Checkpoints are untested.

---
+++ Stealth Racer Game/Assets/Scripts2/CheckpointScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour
+{
+    static int currentCheckpoint = 0;
+    public int thisCheckpoint = 1;
+    public bool isFinalCheckpoint = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Car") //only trigger on collision with a car
+        {
+            if (currentCheckpoint == thisCheckpoint-1) //if the current checkpoint is the last checkpoitn
+            {
+                print("Checkpoint " + thisCheckpoint + " reached");
+                currentCheckpoint = thisCheckpoint;
+                if (isFinalCheckpoint)
+                {
+                    print("Game won!");
+                }
+            }
+        }
+    }
+}
---
+++ Stealth Racer Game/Assets/Scripts2/TimerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static TimerScript timerRef;
+    float timeElapsed = 0;
+    void Start()
+    {
+        timerRef = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("updating time!");
+        timeElapsed += Time.deltaTime;
+        int msec = (int)(timeElapsed * 1000);
+        int seconds = msec / 1000;
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+        msec = msec % 1000;
+        var text = transform.GetChild(0).GetComponent<Text>();
+        text.text = minutes.ToString().PadLeft(2,'0') + ":" + seconds.ToString().PadLeft(2,'0') + ":" + msec.ToString().PadLeft(3,'0');
+    }
+}
