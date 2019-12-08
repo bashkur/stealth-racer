aca2901767fcdca7cb0110adbe38675e58fa8dc2
Bash Kurgunayev <sebastiankurg@gmail.com>
2019-12-07 20:56:35 -0500

    An attempt to parse midi data

---
+++ Stealth Racer Game/Assets/Scripts/MusicTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTester : MonoBehaviour
+{
+    public GameObject sphere_prefab;
+    MusicParser mp = new MusicParser();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DoIt(float time)
+    {
+        Vector3 pos = new Vector3(sphere_prefab.transform.position.x + 1, 0, 0);
+        GameObject g = Instantiate(sphere_prefab, pos, sphere_prefab.transform.rotation);
+    }
+}
